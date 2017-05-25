72506f326be12ce56c8c510a46bb349fabbb0b9a
tokenyc <247067345@qq.com>
2017-05-25 09:06:40 +0800

    基础函数

--- app/src/main/java/net/archeryc/studykotlin/BasicActivity.kt
+++ app/src/main/java/net/archeryc/studykotlin/BasicActivity.kt
@@ -9,12 +9,23 @@ class BasicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic)
-        btn_fun.setOnClickListener { btn_fun.text=sum(2,3).toString() }
+        btn_fun_1.setOnClickListener { btn_fun_1.text = sum(2, 3).toString() }
+        btn_fun_2.setOnClickListener { printSum(2,3) }
     }
 
-    fun sum(a: Int, b: Int): Int {
-        var c=a+b
-        return c
+    /**
+     * 参数名称：参数类型
+     * 返回值名称：参数类型
+     * return 返回值
+     * 表达式为函数体时返回值自动推断
+     */
+    fun sum(a: Int, b: Int) = a + b
+
+    /**
+     * 输出String中的值用$+参数名，$+{表达式}
+     */
+    fun printSum(a: Int,b: Int){
+        println("sum of $a and $b is ${a+b}")
     }
 
 }
